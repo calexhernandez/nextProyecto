3da27ae1c1ef94394b142dfa8f7e9a55ab016874
Cesar Hernandez <cesar.lopex@gmail.com>
2023-03-24 16:10:51 -0600

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+const colors = require('tailwindcss/colors');
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*{html,js,jsx}'],
+  theme: {
+    colors: {
+      ...colors,
+    },
+  },
+};
